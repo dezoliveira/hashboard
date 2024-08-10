a411c68c6a0f786f94054a82bc3b4bae65e16707
Andrés Oliveira <85899665+dezoliveira@users.noreply.github.com>
2024-08-10 03:00:55 +0000

    add: Layout structure

---
+++ src/components/Sidebar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { FaTachometerAlt, FaRegSun, FaChevronRight } from "react-icons/fa";
+
+const Sidebar = () => {
+  return (
+    <div className="bg-cyan-500 h-screen p-4">
+
+    </div>
+  )
+}
+
+export default Sidebar

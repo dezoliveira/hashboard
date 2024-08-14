e558ac3a9506e76d381ace1f3020257169e755fa
Andrés Oliveira <85899665+dezoliveira@users.noreply.github.com>
2024-08-14 02:35:10 +0000

    add: simple line chart recharts

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,52 @@
-import React from "react";
-import { FaRegCalendar } from "react-icons/fa";
+import { FaRegCalendar, FaEllipsisV } from "react-icons/fa";
+
+import React from 'react';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
+
+const data = [
+  {
+    name: 'Page A',
+    uv: 4000,
+    pv: 2400,
+    amt: 2400,
+  },
+  {
+    name: 'Page B',
+    uv: 3000,
+    pv: 1398,
+    amt: 2210,
+  },
+  {
+    name: 'Page C',
+    uv: 2000,
+    pv: 9800,
+    amt: 2290,
+  },
+  {
+    name: 'Page D',
+    uv: 2780,
+    pv: 3908,
+    amt: 2000,
+  },
+  {
+    name: 'Page E',
+    uv: 1890,
+    pv: 4800,
+    amt: 2181,
+  },
+  {
+    name: 'Page F',
+    uv: 2390,
+    pv: 3800,
+    amt: 2500,
+  },
+  {
+    name: 'Page G',
+    uv: 3490,
+    pv: 4300,
+    amt: 2100,
+  },
+];
 
 const Main = () => {
   return (
@@ -41,6 +88,39 @@ const Main = () => {
           <FaRegCalendar fontSize={20}/>
         </div>
 
+      </div>
+
+      <div className="flex mt-[22px] w-full gap-[30px]">
+        <div className="basis-[70%] border bg-white shadow-md cursor-pointer rounded-[4px]">
+          <div className="bg-slate-50 flex items-center justify-between py-[15px] px-[20px] borber-b-[1px] border-slate-50 mb-[20px]">
+            <h2>Earnings Overview</h2>
+            <FaEllipsisV color="gray" className="cursor-pointer"/>
+          </div>
+          <div>
+            <LineChart
+              width={1150}
+              height={500}
+              data={data}
+              margin={{
+                top: 5,
+                right: 30,
+                left: 20,
+                bottom: 5,
+              }}
+            >
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="name" />
+              <YAxis />
+              <Tooltip />
+              <Legend />
+              <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{ r: 8 }} />
+              <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+            </LineChart>
+          </div>
+        </div>
+        <div className="basis-[50%]">
+            
+        </div>
       </div>
     </div>
   )
